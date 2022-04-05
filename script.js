deb2dc70c842cf67a67cabb61ebf70d4f2a81744
Ciro Amore <allbusiness045@gmail.com>
2022-04-05 13:16:36 +0200

    Allow users to choose grid dimensions

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    document.querySelector('.container').appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
-    };
+function getLength() {
+    let input;
+    do {
+        input = +prompt('Size?')
+    } while (input > 100 || input < 0 || !input);
+    return input;
+}
+
+function removeGrid() {
+    let rows = document.querySelectorAll('.row');
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].remove();
+    }
 };
 
-document.querySelectorAll('.square').forEach((square) => {
-    square.addEventListener('mouseover', function() {
-        square.style.backgroundColor = 'black';     
+function addListeners() {
+    document.querySelectorAll('.square').forEach((square) => {
+        square.addEventListener('mouseover', function() {
+            square.style.backgroundColor = 'black';     
+        });
     });
-});
+}
+
+function createGrid(size) {
+    removeGrid();
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        document.querySelector('.container').appendChild(row);
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        };
+    };
+    addListeners();
+};
+
+createGrid(16);
 
 document.querySelector('.eraser').addEventListener('click', function() {
+    let length = getLength();
+    createGrid(length);
     let squares = document.querySelectorAll('.square');
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = 'peachpuff';
-    }
+    };
 });
